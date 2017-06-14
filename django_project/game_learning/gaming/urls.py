236b2e35adabba74bd7ee2318eec07b8c95cd46d
Florian <f.krueger1993@googlemail.com>
2017-06-14 13:29:44 +0200

    Tests, und URLs aufgeräumt

--- django_project/game_learning/gaming/urls.py
+++ django_project/game_learning/gaming/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^game/$', views.game, name='game'),
     url(r'^solution/$', views.solution, name='solution'),
-    url(r'^pdf_game/$', views.pdf_game, name='pdf_game'),
-    url(r'^pdf_solution/$', views.pdf_solution, name='pdf_solution'),
     url(r'^latex_game/$', views.latex_game, name='latex_game'),
     url(r'^latex_solution/$', views.latex_solution, name='latex_solution'),
 ]
